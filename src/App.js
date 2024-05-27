e42a6f85df630fe9a368311e88422dc5338e5c39
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-27 18:34:33 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
+import "./index.css";
 
-function App() {
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Charger", quantity: 1, packed: true },
+// ];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+  
+
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearItems(){
+    const confirmed = window.confirm("Are you sure to delete all the items?");
+
+    if(confirmed) setItems([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItem} />
+      <PackingList
+        items={items}
+        deleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearItems={handleClearItems}
+      />
+      <Stats items={items}/>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Stats.js
@@ -0,0 +1,15 @@
+export default function Stats({items}) {
+  if(!items.length) return <p className="stats"><em>Start adding some items to your packing list 🚀</em></p>
+
+  const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length;
+  const percentPacked = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentPacked === 100 ? 'You got everything! Ready to go 😍' : `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentPacked}%)`}
+      </em>
+    </footer>
+  );
+}
