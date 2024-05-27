78e4506b61fe03ce54c81d9275cae98444d47c39
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-27 18:43:45 +0530

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import Logo from "./components/Logo";
-import Form from "./components/Form";
-import PackingList from "./components/PackingList";
-import Stats from "./components/Stats";
-import "./index.css";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+import "../index.css";
 
 // const initialItems = [
 //   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -13,7 +13,6 @@ import "./index.css";
 
 export default function App() {
   const [items, setItems] = useState([]);
-  
 
   function handleAddItem(item) {
     setItems((items) => [...items, item]);
